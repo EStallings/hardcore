231ce5bd0b96e7a817605ba467e8a3f916e16e2b
Ezra Stallings <ezra@lavu.com>
2015-09-12 15:31:23 -0600

    updated people

--- people.js
+++ people.js
@@ -1,19 +1,54 @@
-C=document.body.appendChild(D=document.createElement("canvas")).getContext("2d");
-D.width = ww=innerWidth;
-D.height = wh=innerHeight;
 
-	function rint(n) { return Math.floor(Math.random()*n); }
+function rint(n) { return Math.floor(Math.random()*n); }
+function easeOutExpo(x) {
+	return -Math.pow( 2, -10 * x ) + 1;
+}
+var outlineWidth = 0.02;
 
-	var outlineWidth = 0.02;
+function rangeVar(start, end) {
+	return Math.random() * (end - start) + start;
+}
 
-	function rangeVar(start, end) {
-		return Math.random() * (end - start) + start;
-	}
+function lerp(v0, v1, t) {
+	return (1-t)*v0 + t*v1;
+}
 
-	function head(C, x, shouldy, bodyHeight, headWidth, headHeight, headGap, headColor, flairColor, shadesType) {
+function smootherstep(x)
+{
+	return x*x*x*(x*(x*6 - 15) + 10);
+}
+
+var tmp = null;
+peopleSprites = {
+	W : tmp=document.createElement("canvas"),
+	C : document.body.appendChild(tmp).getContext("2d"),
+	shirtColors : [
+		'#333',
+		'#444',
+		'#222',
+		'#353535'
+	],
+	flairColors : [
+		'#f80',
+		'#f08',
+		'#8f0',
+		'#0f8',
+		'#80f',
+		'#08f',
+	],
+	skinTones : [
+		'#FFDCB1',
+		'#E5C298',
+		'#CC8443',
+		'#C77A58',
+		'#440000',
+		'#BE723C',
+		'#DFB997'
+	],
+	head : function(C, x, shouldy, bodyHeight, headWidth, headHeight, headGap, headColor, flairColor, shadesType) {
 		C.fillStyle = headColor;
 		C.shadowColor = flairColor;
-		C.shadowBlur = 5;
+		C.shadowBlur = 8;
 		C.fillRect(x, shouldy-bodyHeight-headGap, headWidth, headHeight);
 		C.fillStyle = '#000';
 		switch(shadesType) {
@@ -31,16 +66,15 @@ D.height = wh=innerHeight;
 				C.fillRect(x, shouldy-bodyHeight-headGap+0.2, headWidth, 0.07);
 				C.fillRect(x, shouldy-bodyHeight-headGap+0.3, headWidth, 0.07);
 				break;
-
+			
 		}
-	}
-
-	function bodyPill(C, x, hipy, shouldy, bodyWidth, bodyHeight, shirtColor, flairColor, clothesType) {
+	},
+	bodyPill : function(C, x, hipy, shouldy, bodyWidth, bodyHeight, shirtColor, flairColor, clothesType) {
 		C.shadowColor = flairColor;
-		C.shadowBlur = 5;
+		C.shadowBlur = 8;
 		C.strokeStyle = shirtColor;
 		C.lineWidth = bodyWidth;
-
+	
 		C.beginPath();
 		C.moveTo(x+0.5, hipy+0.9);
 		C.lineTo(x+0.5, shouldy+0.5);
@@ -86,49 +120,22 @@ D.height = wh=innerHeight;
 				break;
 			default:
 				console.err("No clothesType! " + clothesType);
-
+				
 		}
-
-	}
-
-	var shirtColors = [
-		'#333',
-		'#444',
-		'#222',
-		'#353535'
-	]
-
-	var flairColors = [
-		'#f80',
-		'#f08',
-		'#8f0',
-		'#0f8',
-		'#80f',
-		'#08f',
-	]
-
-	var skinTones = [
-		'#FFDCB1',
-		'#E5C298',
-		'#CC8443',
-		'#C77A58',
-		'#440000',
-		'#BE723C',
-		'#DFB997'
-	]
-
-	function makePeople(n) {
-		people = [];
-		for(var i = 1; i < n; i++) {
+	},
+	sprites : [],
+	makePeople : function(n) {
+		this.sprites = [];
+		for(var i = 0; i < n; i++) {
 			var headCvs, bodyCvs;
 			var headCtx = document.body.appendChild(headCvs=document.createElement("canvas")).getContext("2d");
 			var bodyCtx = document.body.appendChild(bodyCvs=document.createElement("canvas")).getContext("2d");
-			headCvs.width = headCvs.height = 64;
+			bodyCvs.width = bodyCvs.height = headCvs.width = headCvs.height = 64;
 			headCtx.scale(32, 32);
 			bodyCtx.scale(32, 32);
-			var shirtColor = shirtColors[rint(shirtColors.length)];
-			var headColor = skinTones[rint(skinTones.length)];
-			var flairColor = flairColors[rint(flairColors.length)];
+			var shirtColor = this.shirtColors[rint(this.shirtColors.length)];
+			var headColor = this.skinTones[rint(this.skinTones.length)];
+			var flairColor = this.flairColors[rint(this.flairColors.length)];
 			var clothesType = rint(4);
 			var shadesType = rint(3);
 
@@ -139,50 +146,50 @@ D.height = wh=innerHeight;
 			var hipy = y+0.1;
 			var shouldy = hipy - bodyHeight;
 			var bodyWidth = rangeVar(0.81, 0.7);
-
+			
 			var headRadius = rangeVar(0.67, 0.70);
 			var headWidth = headRadius;
 			var headHeight = headRadius;
 			var headGap = -0.05;
 
-			bodyPill(bodyCtx, x, hipy, shouldy, bodyWidth, bodyHeight, shirtColor, flairColor, clothesType);
-			head(headCtx, x+(1-headWidth)/2,shouldy, bodyHeight, headWidth, headHeight, headGap, headColor, flairColor, shadesType);
-			people.push({headCvs : headCvs, bodyCvs : bodyCvs});
+			this.bodyPill(bodyCtx, x, hipy, shouldy, bodyWidth, bodyHeight, shirtColor, flairColor, clothesType);
+			this.head(headCtx, x+(1-headWidth)/2,shouldy, bodyHeight, headWidth, headHeight, headGap, headColor, flairColor, shadesType);
+			this.sprites.push({headCvs : headCvs, bodyCvs : bodyCvs, flairColor : flairColor});
 			document.body.removeChild(headCvs);
 			document.body.removeChild(bodyCvs);
 		}
-	}
+	},
 
-	function lerp(v0, v1, t) {
-		return (1-t)*v0 + t*v1;
-	}
-
-	function smootherstep(x)
-	{
-		return x*x*x*(x*(x*6 - 15) + 10);
-	}
-
-	function drawPerson(x, y, i, t) {
-		t = Math.sin(t*0.2); //TODO make this line up with Luke's timing
+	drawPerson : function(x, y, i, t) {
+		t = Math.sin(t*0.25); //TODO make this match your timing
 		var hdx = 1+((i%5)/5-0.5);
 		var hdy = 1+((i%7)/7-0.5);
-
+	
 		var hx = lerp(-hdx, hdx, t);
 		var hy = lerp(-hdy, hdy, t);
 		var hy2 = lerp(-1, 1, t);
 
-		//Use a people-canvas instead of "C"
-		C.drawImage(people[i].bodyCvs, x, y+hy2);
-		C.drawImage(people[i].headCvs, x+hx, y+hy);
+		this.C.drawImage(this.sprites[i].bodyCvs, x, y+hy2);
+		this.C.drawImage(this.sprites[i].headCvs, x+hx, y+hy);
+	},
+	clear : function() {
+		this.C.clearRect(0,0,ww,wh);
 	}
 
-	var frame = 0;
-	makePeople(30); //Instead of 30, use the number of unique people you need
-	function draw() {
-		C.clearRect(0,0,ww,wh); //Do this every frame
-		for(var i = 0; i < people.length; i++) {
-			drawPerson(i*32, 2, i, frame); //drawPerson(x*32, y*32, spriteId, frame)
-		}
-		frame++;
-		requestAnimationFrame(draw);
-	}draw();
+};
+peopleSprites.W.width = ww;peopleSprites.W.height = wh;
+resizableCanvases.push(peopleSprites.W);
+
+
+// peopleSprites.makePeople(30);	
+
+// var frame = 0;
+// function draw() {
+// 	peopleSprites.clear();
+// 	for(var i = 0; i < 30; i++) {
+// 		peopleSprites.drawPerson(i*32, 0, i, frame);
+// 	}
+	
+// 	frame++;
+// 	requestAnimationFrame(draw);
+// }draw();
