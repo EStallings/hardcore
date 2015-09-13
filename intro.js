0a241d20f51e43072929d88136498d1c091582e1
Ezra Stallings <ezra@lavu.com>
2015-09-13 12:55:46 -0600

    fixed spaces, changed mute to esc

--- intro.js
+++ intro.js
@@ -35,14 +35,14 @@ var p4core = image('p4core.png');
 var begin = image('begin.png');
 
 var showIntro = _ => {
-  with(W.getContext('2d')) {
-    rgb(0.9, 0.9, 0.9);
-    shadowBlur = 0;
-    var img = imgSwitch();
+	with(W.getContext('2d')) {
+		rgb(0.9, 0.9, 0.9);
+		shadowBlur = 0;
+		var img = imgSwitch();
 
-    fillRect(-200, -100, 400, 200);
-    drawImage(img, -200, -100);
-  }
+		fillRect(-200, -100, 400, 200);
+		drawImage(img, -200, -100);
+	}
 };
 
 var ctlU;
@@ -52,94 +52,97 @@ var ctlR;
 var ctlB;
 var boundKeys = [];
 introKeyListener = e => {
-  if (e.keyCode === 13) {
-    if (state < 6) {state++;return;}
-    if (state === 16) {init = false; return;}
-    if (state === 21) {init = false; return;}
-    if (state === 26) {init = false; return;}
-    return; //Cannot bind enter to anything else.
-  }
-  if(e.keyCode === 32 && scrubs.length == 0) {
-    state = 26;
-    var P1 = new player(87, 83, 65, 68, 81);
-    var P2 = new player(73, 75, 74, 76, 85);
-  }
-  if(boundKeys[e.keyCode]) return;
-  if(state >= 6) boundKeys[e.keyCode] = true;
+	console.log(e.keyCode);
+	if(e.keyCode === 27) {
+		audio.muted = !audio.muted;
+		return;
+	}
+	if (e.keyCode === 13) {
+		if (state < 6) {state++;return;}
+		if (state === 16) {init = false; return;}
+		if (state === 21) {init = false; return;}
+		if (state === 26) {init = false; return;}
+		return; //Cannot bind enter to anything else.
+	}
+	if(e.keyCode === 32 && scrubs.length == 0) {
+		state = 26;
+		var P1 = new player(87, 83, 65, 68, 81);
+		var P2 = new player(73, 75, 74, 76, 85);
+	}
+	if(boundKeys[e.keyCode]) return;
+	if(state >= 6) boundKeys[e.keyCode] = true;
 
-  if (state === 6) {ctlU = e.keyCode;state++;return;}
-  if (state === 7) {ctlD = e.keyCode;state++;return;}
-  if (state === 8) {ctlL = e.keyCode;state++;return;}
-  if (state === 9) {ctlR = e.keyCode;state++;return;}
-  if (state === 10) {
-    ctlB = e.keyCode;state++;
-    var P1 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
-    return;
-  }
+	if (state === 6) {ctlU = e.keyCode;state++;return;}
+	if (state === 7) {ctlD = e.keyCode;state++;return;}
+	if (state === 8) {ctlL = e.keyCode;state++;return;}
+	if (state === 9) {ctlR = e.keyCode;state++;return;}
+	if (state === 10) {
+		ctlB = e.keyCode;state++;
+		var P1 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
+		return;
+	}
 
-  if (state === 11) {ctlU = e.keyCode;state++;return;}
-  if (state === 12) {ctlD = e.keyCode;state++;return;}
-  if (state === 13) {ctlL = e.keyCode;state++;return;}
-  if (state === 14) {ctlR = e.keyCode;state++;return;}
-  if (state === 15) {
-    ctlB = e.keyCode;state++;
-    var P2 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
-    return;
-  }
+	if (state === 11) {ctlU = e.keyCode;state++;return;}
+	if (state === 12) {ctlD = e.keyCode;state++;return;}
+	if (state === 13) {ctlL = e.keyCode;state++;return;}
+	if (state === 14) {ctlR = e.keyCode;state++;return;}
+	if (state === 15) {
+		ctlB = e.keyCode;state++;
+		var P2 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
+		return;
+	}
 
-  if (state === 16) {ctlU = e.keyCode;state++;return;}
-  if (state === 17) {ctlD = e.keyCode;state++;return;}
-  if (state === 18) {ctlL = e.keyCode;state++;return;}
-  if (state === 19) {ctlR = e.keyCode;state++;return;}
-  if (state === 20) {
-    ctlB = e.keyCode;state++;
-    var P3 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
-    return;
-  }
+	if (state === 16) {ctlU = e.keyCode;state++;return;}
+	if (state === 17) {ctlD = e.keyCode;state++;return;}
+	if (state === 18) {ctlL = e.keyCode;state++;return;}
+	if (state === 19) {ctlR = e.keyCode;state++;return;}
+	if (state === 20) {
+		ctlB = e.keyCode;state++;
+		var P3 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
+		return;
+	}
 
-  if (state === 21) {ctlU = e.keyCode;state++;return;}
-  if (state === 22) {ctlD = e.keyCode;state++;return;}
-  if (state === 23) {ctlL = e.keyCode;state++;return;}
-  if (state === 24) {ctlR = e.keyCode;state++;return;}
-  if (state === 25) {
-    ctlB = e.keyCode;state++;
-    var P4 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
-    return;
-  }
+	if (state === 21) {ctlU = e.keyCode;state++;return;}
+	if (state === 22) {ctlD = e.keyCode;state++;return;}
+	if (state === 23) {ctlL = e.keyCode;state++;return;}
+	if (state === 24) {ctlR = e.keyCode;state++;return;}
+	if (state === 25) {
+		ctlB = e.keyCode;state++;
+		var P4 = new player(ctlU, ctlD, ctlL, ctlR, ctlB);
+		return;
+	}
 };
 
 var imgSwitch = function() {
-
-
-  var img = null;
-  switch (state) {
-    case 0: img = tut1; break;
-    case 1: img = tut2; break;
-    case 2: img = tut3; break;
-    case 3: img = tut4; break;
-    case 4: img = tut5; break;
-    case 5: img = tut6; break;
-    case 6: img = p1up; break;
-    case 7: img = p1down; break;
-    case 8: img = p1left; break;
-    case 9: img = p1right; break;
-    case 10: img = p1core; break;
-    case 11: img = p2up; break;
-    case 12: img = p2down; break;
-    case 13: img = p2left;  break;
-    case 14: img = p2right; break;
-    case 15: img = p2core;  break;
-    case 16: img = p3up;    break;
-    case 17: img = p3down;  break;
-    case 18: img = p3left;  break;
-    case 19: img = p3right; break;
-    case 20:  img = p3core;  break;
-    case 21: img = p4up;    break;
-    case 22: img = p4down;  break;
-    case 23: img = p4left;  break;
-    case 24: img = p4right; break;
-    case 25:  img = p4core;  break;
-    default: img = begin; break;
-  }
-  return img;
+	var img = null;
+	switch (state) {
+		case 0: img = tut1; break;
+		case 1: img = tut2; break;
+		case 2: img = tut3; break;
+		case 3: img = tut4; break;
+		case 4: img = tut5; break;
+		case 5: img = tut6; break;
+		case 6: img = p1up; break;
+		case 7: img = p1down; break;
+		case 8: img = p1left; break;
+		case 9: img = p1right; break;
+		case 10: img = p1core; break;
+		case 11: img = p2up; break;
+		case 12: img = p2down; break;
+		case 13: img = p2left;	break;
+		case 14: img = p2right; break;
+		case 15: img = p2core;	break;
+		case 16: img = p3up;		break;
+		case 17: img = p3down;	break;
+		case 18: img = p3left;	break;
+		case 19: img = p3right; break;
+		case 20:	img = p3core; break;
+		case 21: img = p4up;		break;
+		case 22: img = p4down;	break;
+		case 23: img = p4left;	break;
+		case 24: img = p4right; break;
+		case 25:	img = p4core; break;
+		default: img = begin; break;
+	}
+	return img;
 };
